2c11b4c64b340489360d9176168c0a0de358968b
Shafinul <haqueshafinul@gmail.com>
2024-08-19 17:17:35 -0400

    modify dummy benchmarking script

--- userlib/labscriptlib/main_experiment_control/sequences/dummy_benchmarking.py
+++ userlib/labscriptlib/main_experiment_control/sequences/dummy_benchmarking.py
@@ -3,6 +3,7 @@ from multiprocessing import connection
 from labscript_devices.PrawnBlaster.labscript_devices import PrawnBlaster
 from labscript_devices.NI_DAQmx.models.NI_PXIe_6361 import NI_PXIe_6361
 
+# Import a subsequence function from subsequences.py
 from labscriptlib.main_experiment_control.subsequences.subsequences import absorption_signal
 
 from labscript import (
@@ -44,53 +45,49 @@ ai1_6361 = AnalogIn(name='ai1_6361', parent_device=ni_6361_mio, connection='ai1'
 
 ########## START OF EXPERIMENT LOGIC ################################################
 
-def digital_start_ref():
+# A wrapper of the DO functionality to define an asynchronous start reference pulse
+# It is not relevant to the actual sequence
+def digital_ref():
     t=0
     do1_6361.go_high(t)
     t+=10e-3
     do1_6361.go_low(t)
     return t
 
-def digital_pwm_stream():
-    t=CONTROL_PWM_TI
-    # add_time_marker(t, "D0 PWM CTRL")
-    while t<=CONTROL_PWM_TF:
-        do0_6361.go_high(t)
-        t+=((1/CONTROL_PWM_FREQ) * CONTROL_DUTY_CYCLE)
-        do0_6361.go_low(t)
-        t+=((1/CONTROL_PWM_FREQ) * (1-CONTROL_DUTY_CYCLE))
+# A wrapper for the labscript `acquire` primitive
+def collect_AO(ai_chan: AnalogIn, start_time, end_time):
+    t=start_time
+    t+=ai0_6361.acquire(label='AO_ramp', start_time=t, end_time=end_time)
     return t
 
-def analog_output_ramp(ao_chan: AnalogOut):
-    t=RAMP_TI
-    add_time_marker(t, f"Analog Ramp for {ao_chan.name}", verbose=True)
-    t+=ao_chan.ramp(
-        t=t, 
-        initial=RAMP_VI, 
-        final=RAMP_VF, 
-        duration=RAMP_DUR, 
-        samplerate=RAMP_RATE
-    )
-    ao_chan.constant(t=t, value=0.1)
-    return t
-
-def collect_AO_0():
-    t=RAMP_TI
-    t+=ai0_6361.acquire(label='AO_ramp', start_time=t, end_time=RAMP_TF)
-    return t
-
-def collect_AO_1():
-    t=DIP_TI
-    t+=ai1_6361.acquire(label='AI_signal', start_time=0, end_time=DIP_TF)
-    return t
-
-t=0
+# start the sequence - we will write it to contain an output ramp and absorption signal
+t = 0
 start()
-t=max(t, digital_start_ref())
-t=max(t, digital_pwm_stream())
-t=max(t, analog_output_ramp(ao1_6361))
-t=max(t, absorption_signal(ao0_6361))
-t=max(t, collect_AO_0())
-t=max(t, collect_AO_1())
-print(t)
+
+# write an output ramp, leveraging GLOBALS to define ramp parameters
+t=RAMP_TI
+add_time_marker(t, f"Analog Ramp for {ao_chan.name}", verbose=True)
+t+=ao_chan.ramp(
+    t=t,
+    initial=RAMP_VI,
+    final=RAMP_VF,
+    duration=RAMP_DUR,
+    samplerate=RAMP_RATE
+)
+ao_chan.constant(t=t, value=0)
+
+# use subsequences to define the next part of the sequence
+# NOTE: the timing of the absorption signal is defined in the SUBSEQUENCES GLOBALS
+# but if it is more intuitive, there is no reason it can't be set in the sequence script itself
+absorption_end_time = absorption_signal(ao0_6361)
+t = max(t, absorption_end_time) 
+
+# make asycnhronous call to the digital reference signal
+digital_ref_end_time = digital_ref()
+t=max(t, digital_ref_end_time)
+
+# collect signals - again leveraging GLOBALS to know when the signal should be acquired
+collect_AO(ai0_6361, RAMP_TI, RAMP_TF)
+collect_AO(ai1_6361, DIP_TI, DIP_TF)
+
 stop(t)
